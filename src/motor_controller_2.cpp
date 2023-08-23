03532c6ed59f6c47d524e817d22c1749f6b46193
Saydul Mursalin Peash <saydulpeash428@gmail.com>
2023-08-23 15:26:36 +0600

    wywse5u

--- src/motor_controller_2.cpp
+++ src/motor_controller_2.cpp
@@ -11,28 +11,7 @@ void commandsCllback(const std_msgs::String::ConstPtr &msg){
     float l_speed=0;
     float r_speed=0;
     std::string c=msg->data;
-
-    if(c=="GO"){
-        l_speed=-1.0;
-        r_speed=-1.0;
-    }else if(c=="GO_REALLY_FAST"){
-        l_speed=-10;                //radian per sec
-        r_speed=-10;
-
-    }else if(c=="BACK"){
-        l_speed=0.5;
-        r_speed=0.5;
-
-    }else if(c=="LEFT"){
-        l_speed=-0.5;
-        r_speed=-1.0;
-    }else if(c=="RIGHT"){
-        l_speed=-1.0;
-        r_speed=-0.5;
-    }else{
-        l_speed=0;
-        r_speed=0;
-    }
+    
     std_msgs::Float64 msgLeft,msgRight;
     msgLeft.data=l_speed;
     msgRight.data=r_speed;
@@ -42,7 +21,7 @@ void commandsCllback(const std_msgs::String::ConstPtr &msg){
 
 
 int main(int argc,char **argv){
-    ros::init(argc,argv,"motorController");
+    ros::init(argc,argv,"motorController2");
     ros::NodeHandle n;
     ros::Subscriber commandSub=n.subscribe("motor_commands",1000,commandsCllback);
     leftPub=n.advertise<std_msgs::Float64>("/left_wheel_controller/command",1000);
